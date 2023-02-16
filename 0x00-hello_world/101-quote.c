1b321960b19afe5508a75ed3a40d313c2b00dcf7
Castro Hamiltone <castromiltone@gmail.com>
2023-02-16 14:31:19 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fprintf(stderr, "%s", s);
 	return (1);
