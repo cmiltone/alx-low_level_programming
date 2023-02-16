df883413d3a818a8d1010caefb67024773bb961f
Castro Hamiltone <castromiltone@gmail.com>
2023-02-16 14:08:22 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,16 @@
-/*
- * C Program To demonstrate
- * Size of operators
- *
+#include <stdio.h>
+
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
-*/
-#include <stdio.h>
+ */
 int main(void)
 {
-	printf("Size of a char: %lu\n", sizeof(char));
-	printf("Size of an int: %lu\n", sizeof(int));
-	printf("Size of a long int: %lu\n", sizeof(long int));
-	printf("Size of a long long int: %lu\n", sizeof(long long int));
-	printf("Size of a float: %lu\n", sizeof(float));
-	printf("%lu", sizeof(double));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
