aad782ef586e40db94ec95f5209ae3d2f10babd6
Castro Hamiltone <castromiltone@gmail.com>
2023-02-21 13:26:16 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: void
+ */
+
+void main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i <= 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum);
+}
