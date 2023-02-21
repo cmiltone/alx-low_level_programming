2c4168a53aade1e1e43576f71fdc5ff2cfbcdae9
Castro Hamiltone <castromiltone@gmail.com>
2023-02-21 12:12:13 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * times_table - prints 9 time table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int len;
+	char buff[80];
+	int comma = ',';
+	int space = ' ';
+	int newline = '\n';
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i * j < 10)
+			{
+				write(1, &space, 1);
+			}
+			len = sprintf(buff, "%d", i * j);
+			write(1, buff, len);
+			if (j < 9)
+			{
+				write(1, &comma, 1);
+				write(1, &space, 1);
+			}
+		}
+		write(1, &newline, 1);
+	}
+}
