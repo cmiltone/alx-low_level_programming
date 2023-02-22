4fd926f1b7e31efbd6e256e074c4afd63520449f
Castro Hamiltone <castromiltone@gmail.com>
2023-02-21 18:57:22 -0800

    fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,18 +11,41 @@
 void fibonacci(int n)
 {
 	static unsigned long int a = 0, b = 1;
-	unsigned long int c;
+	unsigned long int c, h1, h2;
+	unsigned long int a1, a2, b1, b2;
 
 	if (n > 0)
 	{
 		c = a + b;
 		a = b;
 		b = c;
+		if (n > 6)
+		{
+			if (n == 1)
+				printf("%lu\n", c);
+			else
+				printf("%lu, ", c);
+		} else
+		{
+			a1 = a / 10000000000;
+			b1 = b / 10000000000;
+			a2 = a % 10000000000;
+			b2 = b % 10000000000;
+			h1 = a1 + b1;
+			h2 = a2 + b2;
+			if (a2 + b2 > 9999999999)
+			{
+				h1 += 1;
+				h2 %= 10000000000;
+			}
+			printf("%lu%lu, ", h1, h2);
 
-		if (n == 1)
-			printf("%lu\n", c);
-		else
-			printf("%lu, ", c);
+			a1 = b1;
+			a2 = b2;
+			b1 = h1;
+			b2 = h2;
+		}
+
 		fibonacci(n - 1);
 	}
 }
@@ -37,7 +60,7 @@ int main(void)
 {
 	int n = 98;
 
-	fibonacci(n - 6);
+	fibonacci(n);
 
 	return (0);
 }
