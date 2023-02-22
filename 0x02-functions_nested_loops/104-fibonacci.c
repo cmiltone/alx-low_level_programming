74b6124d7d6acb289ac5b3536becb350257e43d1
Castro Hamiltone <castromiltone@gmail.com>
2023-02-21 16:57:41 -0800

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h> 
+
+/**
+ * fibonacci - prints fibonacci sequence
+ *
+ * @n: the number
+ *
+ * Return: int
+ */
+
+int fibonacci(int n)
+{
+	static unsigned long int a = 0, b = 1;
+	unsigned long int c;
+
+	if (n > 0)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		if (n == 1)
+			printf("%lu\n", c);
+		else
+			printf("%lu, ", c);
+		fibonacci(n - 1);
+	}
+}
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	fibonacci(98);
+
+	return (0);
+}
