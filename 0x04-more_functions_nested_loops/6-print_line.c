cbcc9b4fad17acb46338a620bc6339c173af27ff
Castro Hamiltone <castromiltone@gmail.com>
2023-02-23 14:02:09 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+/**
+ * print_line - prints a line
+ *
+ * @n: a number
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int k;
+	int underscore = '_';
+	int newline = '\n';
+
+	for (k = 1; k <= n; k++)
+	{
+		write(1, &underscore, 1);
+	}
+	write(1, &newline, 1);
+}
