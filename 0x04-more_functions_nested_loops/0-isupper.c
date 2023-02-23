978d3eb40143bf2b84a273990eac827e936240fd
Castro Hamiltone <castromiltone@gmail.com>
2023-02-23 12:49:46 -0800

    0x04-more_functions_nested_loops - 0-isupper.c'

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+/**
+ * _isupperer - checks if charter is uppercase
+ *
+ * @c: - int
+ *
+ * Return: returns 0 if lowercase  or 1 if uppercase
+ *
+ */
+
+int _isupperer(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	} else if (c >= 97 && c <= 122)
+	{
+		return (0);
+	}
+	return (0);
+}
+
