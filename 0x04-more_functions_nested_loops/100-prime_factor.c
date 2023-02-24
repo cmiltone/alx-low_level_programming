35d86b135698520526de809784eca69803f1046b
Castro Hamiltone <castromiltone@gmail.com>
2023-02-24 10:19:30 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	long int num = 612852475143, d = 2, max;
+
+	while (num != 0)
+	{
+		if (num % d != 0)
+		{
+			d += 1;
+		} else
+		{
+			max = num;
+			num /= d;
+			if (num == 1)
+			{
+				printf("%ld", max);
+				break;
+			}
+		}
+	}
+	return (0);
+}
