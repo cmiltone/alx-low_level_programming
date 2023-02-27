7c0a79f129fa0db659ec29128109b8eb73350a74
Castro Hamiltone <castromiltone@gmail.com>
2023-02-27 09:51:52 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * print_rev - prints a string in reverse order
+ *
+ * @s: the string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len  = strlen(s), i;
+	char c;
+	int newline = '\n';
+
+	for (i = len; i >= 0; i--)
+	{
+		c = s[i];
+		write(1, &c, 1);
+	}
+	write(1, &newline, 1);
+}
