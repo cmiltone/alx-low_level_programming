263b10efa24d4496fa3d0308efab2983e498268e
Castro Hamiltone <castromiltone@gmail.com>
2023-02-27 11:37:02 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * puts_half - prints half a string
+ *
+ * @str: string
+ *
+ * Return: void
+ *
+ */
+
+void puts_half(char *str)
+{
+	int len  = strlen(str);
+	int n, i, c, newline = '\n';
+
+	if (len % 2 != 0)
+	{
+		n = (len - 1) / 2;
+	} else
+	{
+		n = len / 2;
+	}
+	for (i = n; i < len; i++)
+	{
+		c = str[i];
+		write(1, &c, 1);
+	}
+	write(1, &newline, 1);
+}
