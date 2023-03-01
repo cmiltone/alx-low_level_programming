164963450085e5963539a5f5d53038f0a3cdb8dd
Castro Hamiltone <castromiltone@gmail.com>
2023-03-01 12:20:37 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include <string.h>
+
+/**
+ * _strcmp - compares strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: int
+ */
+char *_strcmp(char *s1, char *s2)
+{
+	int r;
+
+	r = strcmp(s1, s2);
+
+	return (r);
+}
