318cad8c7e4e34b9357d07a40a99e7cafe90cc8b
Castro Hamiltone <castromiltone@gmail.com>
2023-03-02 12:16:25 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * cap_string - capitalize string
+ *
+ * @s: string
+ *
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+	int i, len = strlen(s), c, diff = 32;
+	char *s2 = malloc(len * sizeof(char));
+
+	for (i = 1; i < len; i++)
+	{
+		c = s[i - 1];
+		if (c == 32 || c == 9 || c == 10 || c == 44 || c == 59 ||
+				c == 46 || c == 33 || c == 63 || c == 34 ||
+				c == 40 || c == 41 || c == 123 || c == 125)
+		{
+			if (c >= 97 && i <= 122)
+			{
+				s2[i] = s[i] - diff;
+			} else
+			{
+				s2[i] = s[i];
+			}
+			printf("s: %s\n", s2);
+		} else
+		{
+			s2[i] = s[i];
+		}
+	}
+	printf("%s\n", s2);
+	strcpy(s, s2);
+
+	return (s);
+}
