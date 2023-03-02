3ae036906be08c4b549d0fff27cbfcce0cc08428
Castro Hamiltone <castromiltone@gmail.com>
2023-03-02 11:33:34 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * string_toupper - converts string to uppercase
+ *
+ * @s: string
+ *
+ * Return: string
+ */
+
+char *string_toupper(char *s)
+{
+	int diff = 32, n = strlen(s), i;
+	char *s2 = malloc(n * sizeof(char));
+	int c;
+
+	for (i = 0; i < n; i++)
+	{
+		c = s[i];
+
+		if (c >= 97 && i <= 122)
+		{
+			s2[i] = s[i] - diff;
+		} else
+		{
+			s2[i] = s[i];
+		}
+	}
+	strcpy(s, s2);
+
+	return (s);
+}
