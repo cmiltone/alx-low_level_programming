7c4b4a452474694033c5c542c579fa5026c61c45
Castro Hamiltone <castromiltone@gmail.com>
2023-03-08 13:25:49 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns length of string
+ *
+ * @s: string
+ *
+ * Return: int
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		return 0;
+	}
+
+	return _strlen_recursion(s + 1) + 1;
+}
