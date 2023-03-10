c24c2b0dadb8222d9a5fab2a1fca5072dce15308
Castro Hamiltone <castromiltone@gmail.com>
2023-03-10 11:45:38 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments
+ * @argv: array or arguments
+ *
+ * Return: always zero
+ */
+
+int main(int argc, char **argv)
+{
+	while(argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+
+	return (0);
+}
