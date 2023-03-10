152723a8a80df873f03b8f294bb4c61daa04410e
Castro Hamiltone <castromiltone@gmail.com>
2023-03-10 11:39:09 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc - number of arguments
+ * @argv - array or arguments
+ *
+ * Return: always zero
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	(void)argv;
+
+	return (0);
+}
