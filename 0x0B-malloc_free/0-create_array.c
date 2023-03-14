823d3ead109d7b257314ad82b84e619b134d3629
Castro Hamiltone <castromiltone@gmail.com>
2023-03-14 09:50:27 -0700

    0-create_array.c - fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,8 +9,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	char *ptr = malloc(size * sizeof(char));
 	unsigned int i;
+
+	if (!ptr)
+	{
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 	{
