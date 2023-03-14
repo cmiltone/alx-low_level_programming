5641901042e8595eff5ad1503b40910cd8561da4
Castro Hamiltone <castromiltone@gmail.com>
2023-03-14 11:47:12 -0700

    2-str_concat.c - fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,14 +1,10 @@
 #include <string.h>
 #include <stdlib.h>
-
 /**
  * str_concat - concatenates two strings
- *
  * @s1: string one
  * @s2: string two
- *
  * Return: string
- *
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -16,40 +12,32 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-	}
-
-
+		return ("");
 	if (s1 != NULL && s2 != NULL)
 	{
 		s = malloc((strlen(s1) + strlen(s2)) * sizeof(char));
+		if (s == NULL)
+			return (NULL);
 		for (i = 0; i < strlen(s1); i++)
-		{
 			s[i] = s1[i];
-		}
-		
 		for (i = strlen(s1); i < strlen(s1) + strlen(s2); i++)
-		{
 			s[i] = s2[i - strlen(s1)];
-		}
 	}
 	if (s1 != NULL && s2 == NULL)
 	{
 		s = malloc(strlen(s1) * sizeof(char));
+		if (s == NULL)
+			return (NULL);
 		for (i = 0; i < strlen(s1); i++)
-		{
 			s[i] = s1[i];
-		}
 	}
 	if (s2 != NULL && s1 == NULL)
 	{
 		s = malloc(strlen(s2) * sizeof(char));
+		if (s == NULL)
+			return (NULL);
 		for (i = 0; i < strlen(s2); i++)
-		{
 			s[i] = s2[i];
-		}
 	}
-
 	return (s);
 }
