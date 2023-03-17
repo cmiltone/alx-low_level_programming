beac66371d809a172feb3ee67dadaf759781ffeb
Castro Hamiltone <castromiltone@gmail.com>
2023-03-17 22:34:18 +0300

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,13 +9,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 	void *arr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	arr = calloc(nmemb, size);
+	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
-
+	for (i = 0; i < nmemb * size; i++)
+	{
+		((char *)arr)[i] = 0;
+	}
 	return (arr);
 }
