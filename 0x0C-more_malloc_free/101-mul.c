8daba591635bf3fb06804a03613190578c3eb9ea
Castro Hamiltone <castromiltone@gmail.com>
2023-03-17 16:15:15 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
+/**
+ * main - entry point
+ * @n: number of arguments
+ * @arr: array of values
+ *
+ * Return: void
+ */
+
+void main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!isdigit(*argv[1]) || !isdigit(*argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+}
