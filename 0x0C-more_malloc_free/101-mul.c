0f42fe118b1d56c99320615640d6d44efdc37c58
Castro Hamiltone <castromiltone@gmail.com>
2023-03-17 17:00:24 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,26 +1,76 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <unistd.h>
+#include <stdio.h>
 
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_char(char c);
+void print_error();
+void _print_int(int n);
+
+#endif
 /**
  * main - entry point
  * @argc: number of arguments
  * @argv: array of values
  *
- * Return: void
+ * Return: always zero
  */
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("Error\n");
+		print_error();
 		exit(98);
 	}
 	if (!isdigit(*argv[1]) || !isdigit(*argv[2]))
 	{
-		printf("Error\n");
+		print_error();
 		exit(98);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	_print_int(atoi(argv[1]) * atoi(argv[2]));
+	print_char('\n');
+	return (0);
+}
+
+/**
+ * print_char - prints character
+ * @c: character
+ * Return: void
+ */
+void print_char(char c)
+{
+	write(1, &c, 1);
+}
+
+/**
+ * print_error - prints error string
+ * Return: void
+ */
+void print_error()
+{
+	print_char('E');
+	print_char('r');
+	print_char('r');
+	print_char('o');
+	print_char('r');
+	print_char('\n');
+}
+
+/**
+ * print_int - prints integer
+ * @n: integer
+ * Return: void
+ */
+
+void _print_int(int n)
+{
+	int l;
+	char buff[1000000000000000000];
+
+	l = sprintf(buff, "%d", n);
+	write(1, buff, l);
 }
