25a9b6d5dd0a50174b1038662fa32430672882fd
Castro Hamiltone <castromiltone@gmail.com>
2023-03-18 03:38:21 +0300

    fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 #ifndef MAIN_H
 #define MAIN_H
@@ -9,6 +10,8 @@
 void print_char(char c);
 void print_error();
 void _print_int(int n);
+char *int_to_string(int n);
+char *strrev(char *str);
 
 #endif
 /**
@@ -61,6 +64,35 @@ void print_error()
 }
 
 /**
+ * int_to_string - converts integer to string
+ * @n: integer
+ * Return: string
+ */
+
+char *int_to_string(int n)
+{
+	int i, sign;
+	char *s;
+
+	s = malloc(n * sizeof(char));
+	if (s == NULL)
+		return ("");
+	
+	if ((sign = n) < 0)
+		n = -n;
+	i = 0;
+	do
+	{ 
+		s[i++] = n % 10 + '0';
+	} while ((n /= 10) > 0);
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	strrev(s);
+	return (s);
+}
+
+/**
  * print_int - prints integer
  * @n: integer
  * Return: void
@@ -68,9 +100,32 @@ void print_error()
 
 void _print_int(int n)
 {
-	int l;
-	char buff[1000000000000000000];
+	char *s = int_to_string(n);
+	int i, l = strlen(s);
 
-	l = sprintf(buff, "%d", n);
-	write(1, buff, l);
+	for (i = 0; i < l; i++)
+	{
+		print_char(s[i]);
+	}
+}
+
+/**\
+ * strrev - reverse string
+ * @str - the string
+ * Return: string
+*/
+
+char *strrev(char *str)
+{
+	char *p1, *p2;
+	
+	if (! str || ! *str)
+	return (str);
+	for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
+	{
+		*p1 ^= *p2;
+		*p2 ^= *p1;
+		*p1 ^= *p2;
+	}
+	return (str);
 }
