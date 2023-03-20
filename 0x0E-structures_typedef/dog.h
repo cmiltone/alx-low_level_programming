e66fbb68ace43ece9dfaa53c45524ae47c73be28
Castro Hamiltone <castromiltone@gmail.com>
2023-03-21 02:26:10 +0300

    betty fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 typedef struct dog dog_t;
