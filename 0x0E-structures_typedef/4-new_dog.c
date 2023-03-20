930ae8f8eb5c46fb38fa3704775bf41a4ea73e61
Castro Hamiltone <castro@moovn.com>
2023-03-21 01:54:20 +0300

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* dog_t  - creates a new dog instance and returns it
+* @name: name of dog,
+* @age: age, 
+* @owner: owner
+* Return: dog instance
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    dog_t *d = malloc(sizeof(dog_t));
+
+    if (d == NULL)
+        return (NULL);
+
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
+    
+    return (d);
+}
