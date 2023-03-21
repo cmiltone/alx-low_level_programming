6b309a460bfb1b0983bbf7f49060fc3d4ff2f371
Castro Hamiltone <castromiltone@gmail.com>
2023-03-21 21:56:58 +0300

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,40 +17,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (d == NULL)
 		return (NULL);
-
 	dog_name = malloc((strlen(name) + 1) * sizeof(char));
-
 	if (dog_name == NULL)
 	{
 		free(d);
 		free(dog_name);
 		return (NULL);
 	}
-
 	dog_owner = malloc((strlen(owner) + 1) * sizeof(char));
-
 	if (dog_owner == NULL)
 	{
 		free(d);
 		free(dog_owner);
 		return (NULL);
 	}
-
 	k = strlen(name);
-
 	for (i = 0; i < k; i++)
 		dog_name[i] = name[i];
 	dog_name[i] = '\0';
-
 	k = strlen(owner);
-
 	for (i = 0; i < k; i++)
 		dog_owner[i] = owner[i];
 	dog_owner[i] = '\0';
-
 	d->name = dog_name;
 	d->age = age;
 	d->owner = dog_owner;
-
 	return (d);
 }
