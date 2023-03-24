af81aff8d0e27565716889f4116f1f0bd17258dc
Castro Hamiltone <castromiltone@gmail.com>
2023-03-25 00:58:40 +0300

    fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 /**
  * print_name - prints a name
  * @name: the name
@@ -7,5 +8,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	(*f)(name);
 }
