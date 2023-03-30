412b999632cb6e72b83bf34ebd1144086370f4ff
Castro Hamiltone <castromiltone@gmail.com>
2023-03-30 20:32:47 +0300

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - prints elements of a list
+ * @h: the list
+ * Return: number of nodes
+*/
+
+size_t list_len(const list_t *h)
+{
+	list_t *n = (list_t *)h;
+	int i = 0;
+
+	while (n != NULL)
+	{
+		n = n->next;
+		i += 1;
+	}
+
+	return (i);
+}
