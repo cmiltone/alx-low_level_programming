820e28df6e4b2ca61829ffad9a2b3892ae8c8eee
Castro Hamiltone <castromiltone@gmail.com>
2023-04-04 17:45:15 +0300

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees mem from list
+ * @head: the list
+ * Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmpNode;
+
+	if (head == NULL)
+		return;
+
+	while ((*head) != NULL)
+	{
+		tmpNode = (*head);
+		(*head) = (*head)->next;
+		free(tmpNode);
+	}
+
+  head = NULL;
+}
