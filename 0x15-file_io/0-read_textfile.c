64c41d0625dd838f71ab8b28a25936f739332127
Castro Hamiltone <castromiltone@gmail.com>
2023-04-08 17:44:55 +0300

    fix 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -33,9 +33,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	read(f, c, letters);
 
+	n = write(1, c, strlen(c));
+
 	close(f);
 
-	n = write(1, c, strlen(c));
+	free(c);
 
 	if ((int)n == -1)
 		return (0);
