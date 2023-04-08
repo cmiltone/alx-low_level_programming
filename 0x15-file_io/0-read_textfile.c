7e275c56bdd1b92a813c1d0d74f32f20ac46c5ec
Castro Hamiltone <castromiltone@gmail.com>
2023-04-08 17:07:41 +0300

    fixes - 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -20,23 +21,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	c = malloc(sizeof(char) * letters);
-
-	if (c == NULL)
-		return (0);
-
 	f = open(filename, O_RDONLY);
 
 	if (f == -1)
 		return (0);
 
+	c = malloc(sizeof(char) * letters);
+
+	if (c == NULL)
+		return (0);
+
 	read(f, c, letters);
 
-	while (n < letters)
-	{
-		write(1, &c[n], 1);
-		n += 1;
-	}
+	n = write(1, c, strlen(c));
 
 	return (n);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,8 @@
-#include <stdlib.h>
-#include <sys/types.h>
-
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdlib.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
