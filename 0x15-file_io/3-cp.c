03bf486f505eedc85a45470342d18f316615454f
Castro Hamiltone <castromiltone@gmail.com>
2023-04-08 20:52:16 +0300

    betty fixes - 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,43 +21,33 @@ int main(int argc, char *argv[])
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	from = argv[1];
 	to = argv[2];
-
 	f1 = open(from, O_RDONLY);
 	n = read(f1, c, 1024);
-
 	if (n == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", from);
 		exit(98);
 	}
-
 	f2 = open(to, O_RDWR | O_TRUNC | O_CREAT, 0664);
-
 	n = write(f2, c, strlen(c) - 1);
-
 	if (n == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", to);
 		exit(99);
 	}
-
 	n = close(f1);
-
 	if (n == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", f1);
 		exit(99);
 	}
 	n = close(f2);
-
 	if (n == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", f2);
 		exit(99);
 	}
-
 	return (0);
 }
