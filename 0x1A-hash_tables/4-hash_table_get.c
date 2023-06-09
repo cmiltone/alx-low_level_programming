37139b8804e1a72f8406b7b65fd588c49862b9bf
Castro Hamiltone <castromiltone@gmail.com>
2023-06-09 23:51:05 +0300

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves value associated
+ * with a key in hash table
+ * @ht: hash table
+ * @key: the key
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *node = ht->array[index];
+
+	if (node == NULL)
+		return (NULL);
+
+	return (node->value);
+}
