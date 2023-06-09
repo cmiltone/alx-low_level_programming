4c9871a709f78e9f4216a37a82f52cf4ff45437b
Castro Hamiltone <castromiltone@gmail.com>
2023-06-09 22:19:55 +0300

    codestyle fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,6 +4,7 @@
  * key_index - returns index of a key
  * @key: the key
  * @size: the size of the array of the hash table
+ * Return: index of key
 */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
