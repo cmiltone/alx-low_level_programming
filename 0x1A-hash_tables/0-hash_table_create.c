620714bdab774d69552a40fc9a55ff64950cb61f
Castro Hamiltone <castromiltone@gmail.com>
2023-06-09 15:10:55 +0300

    fixes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return NULL;
 
 	hash_table->size = size;
-	hash_table->array = (hash_node_t**)calloc(hash_table->size, sizeof(hash_node_t));
+	hash_table->array = (hash_node_t**)calloc(hash_table->size, sizeof(hash_node_t*));
 	while (i < size)
 	{
 		hash_table->array[i] = NULL;
