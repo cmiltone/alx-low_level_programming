58bc9e36ab64a0b919b96eed0b6ae79ee3923e61
Castro Hamiltone <castromiltone@gmail.com>
2023-06-09 23:59:29 +0300

    4-hash_table_get.c - fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -17,6 +17,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	index  = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 	node  = ht->array[index];
 
 	while (node && strcmp(node->key, key) != 0)
