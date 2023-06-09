5e7d7093b1c887c2017577cbfa46260099427a26
Castro Hamiltone <castromiltone@gmail.com>
2023-06-10 00:38:03 +0300

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - prints hash table values
+ * @ht: hash table
+ * Return: nothing
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	long unsigned int i;
+	int f = 0;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++) {
+		if (ht->array[i] != NULL) {
+			if (f == 1)
+				printf(", ");
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			f = 1;
+		}
+	}
+	printf("}\n");
+}
