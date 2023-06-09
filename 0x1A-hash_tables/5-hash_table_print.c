fce1dad77443a176b789b041bc262849ce069c19
Castro Hamiltone <castromiltone@gmail.com>
2023-06-10 00:41:31 +0300

    fixes -  5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,6 +12,8 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *node;
 	unsigned long int i;
 	int f = 0;
+	if (ht == NULL)
+		return;
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
